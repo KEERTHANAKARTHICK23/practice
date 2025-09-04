ec06b96d7f8ae4794a6cebe8ca2df1b9398afbbc
Keerthana Karthick <keerthanakarthick2346@gmail.com>
2025-09-04 10:25:52 +0530

    add program

---
+++ Week-1/multiply.java
@@ -0,0 +1,11 @@
+public class multiply{
+    public static void main(String[] args) {
+        for(int i=1; i<=16; i++) {
+            for(int k=1; k<=16; k++) {
+
+                System.out.print(i*k + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
